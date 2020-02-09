148f5010d4fcca4cedf56e7ed3703b584279d666
PARK SUNG DONG <oraedoa@gmail.com>
2020-02-09 18:45:18 +0900

    Update MaxProduct.java

--- week6/pasudo123/MaxProduct.java
+++ week6/pasudo123/MaxProduct.java
@@ -1,31 +1,33 @@
 package week06;
 
+import java.math.BigInteger;
+
 public class MaxProduct {
 
     public static void main(String[]args) {
 
         // 1,null,2,3,4,null,null,5,6
 
-//        final TreeNode root = new TreeNode(1);
-//        root.right = new TreeNode(2);
-//        root.right.left = new TreeNode(3);
+        final TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
 //        root.right.right = new TreeNode(4);
 //        root.right.right.left = new TreeNode(5);
 //        root.right.right.right = new TreeNode(6);
 
-        final TreeNode root = new TreeNode(2);
-        root.left = new TreeNode(3);
-        root.right = new TreeNode(9);
-
-        root.left.left = new TreeNode(10);
-        root.left.right = new TreeNode(7);
+//        final TreeNode root = new TreeNode(2);
+//        root.left = new TreeNode(3);
+//        root.right = new TreeNode(9);
+//
+//        root.left.left = new TreeNode(10);
+//        root.left.right = new TreeNode(7);
 //        root.right.left = new TreeNode(8);
-        root.right.right = new TreeNode(6);
-
-        root.left.left.left = new TreeNode(5);
+//        root.right.right = new TreeNode(6);
+//
+//        root.left.left.left = new TreeNode(5);
 //        root.left.left.right = new TreeNode(4);
 //        root.left.right.left = new TreeNode(11);
-        root.left.right.right = new TreeNode(1);
+//        root.left.right.right = new TreeNode(1);
 
         System.out.println(maxProduct(root));
     }
@@ -40,16 +42,18 @@ public class MaxProduct {
         }
     }
 
-    static final int MOD = 1000000007;
-    static int maxValue = -1;
-    static long total = 0;
+    static final String MOD = "1000000007";
+    static BigInteger maxValue = BigInteger.ZERO;
+    static BigInteger total = BigInteger.ZERO;
 
     public static int maxProduct(TreeNode root) {
 
         circuit(root);
         calculate(root);
 
-        return maxValue;
+        System.out.println(total);
+
+        return maxValue.mod(new BigInteger(MOD)).intValue();
     }
 
     static void circuit(TreeNode node){
@@ -58,32 +62,28 @@ public class MaxProduct {
             return;
         }
 
-        total += node.val;
+        total = total.add(BigInteger.valueOf(node.val));
         circuit(node.left);
         circuit(node.right);
     }
 
-    private static int calculate(TreeNode node) {
+    private static BigInteger calculate(TreeNode node) {
 
         if(node == null) {
-            return 0;
+            return BigInteger.ZERO;
         }
 
-        int leftTotal = calculate(node.left) % MOD;
-        int rightTotal = calculate(node.right) % MOD;
-        int subTotal = (node.val + leftTotal + rightTotal) % MOD;
+        BigInteger leftTotal = calculate(node.left);
+        BigInteger rightTotal = calculate(node.right);
 
-        maxValue = (maxValue > (int)mod(subTotal)) ? maxValue : (int)mod(subTotal);
+        BigInteger value = BigInteger.valueOf(node.val);
+        BigInteger subTotal = value.add(leftTotal).add(rightTotal);
+
+        BigInteger subTotalValue = total.subtract(subTotal);
+        subTotalValue = subTotalValue.multiply(subTotal);
+
+        maxValue = (maxValue.compareTo(subTotalValue) < 0) ? subTotalValue : maxValue;
 
         return subTotal;
-    }
-
-    private static long mod(final int subTotal){
-
-
-        long value = (total - (subTotal)) * subTotal;
-        System.out.println((total - (subTotal)) + " * " + subTotal + " = " + value);
-
-        return (value % MOD);
     }
 }
