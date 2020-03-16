6c6778d24bdfcf380da11543cc992862d6b93e0f
dhpark <dhpark0648@gmail.com>
2020-03-16 23:25:48 +0900

    solve: week1 problems

--- week01/dhparkhub/problem1949/Solution.java
+++ week01/dhparkhub/problem1949/Solution.java
@@ -5,87 +5,89 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Resolver resolver = new Resolver();
+        int t, n, k;
+        int[][] map = new int[8][8];
+
         Scanner scanner = new Scanner(System.in);
-        int tc = scanner.nextInt();
-        for (int i=1; i<=tc; i++) {
-            solution.n = scanner.nextInt();
-            solution.k = scanner.nextInt();
-
-            solution.init();
-
-            for (int r=0; r<solution.n; r++) {
-                for (int c=0; c<solution.n; c++) {
-                    solution.map[r][c] = scanner.nextInt();
-                    solution.h = Math.max(solution.h, solution.map[r][c]);
+        t = scanner.nextInt();
+        for (int i = 1; i <= t; i++) {
+            n = scanner.nextInt();
+            k = scanner.nextInt();
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    map[r][c] = scanner.nextInt();
                 }
             }
-
-            int answer = solution.solve();
+            resolver.init(n, k, map);
+            int answer = resolver.resolve();
             System.out.println(String.format("#%d %d", i, answer));
         }
     }
 
-    private void init() {
-        h = 0;
-        for (int r=0; r<n; r++) {
-            for (int c=0; c<n; c++) {
-                visited[r][c] = false;
-            }
-        }
-    }
+    static class Resolver {
 
-    private boolean valid(int r, int c) {
-        return r >= 0 && r < n && c >= 0 && c < n;
-    }
+        final int[] MR = {0, 0, -1, 1};
+        final int[] MC = {-1, 1, 0, 0};
 
-    private int getMaxPath(int r, int c, boolean cheat) {
-//        String out = String.format("(%d %d %s)", r, c, cheat ? "true" : "false");
-//        System.out.println(out);
+        int n, k, h;
+        int[][] map = new int[8][8];
+        boolean[][] visited = new boolean[8][8];
 
-        visited[r][c] = true;
-        int temp = 0;
-        for (int i=0; i<4; i++) {
-            int mr = r + MR[i];
-            int mc = c + MC[i];
-            if (!valid(mr, mc)) continue;
-            if (visited[mr][mc]) continue;
-            if (map[mr][mc] < map[r][c]) {
-                temp = Math.max(temp, getMaxPath(mr, mc, cheat));
-                continue;
-            }
-            if (cheat) continue;
-            for (int j=1; j<=k; j++) {
-                if (map[mr][mc] - j >= 0 && map[mr][mc] - j < map[r][c]) {
-                    map[mr][mc] -= j;
-                    temp = Math.max(temp, getMaxPath(mr, mc, true));
-                    map[mr][mc] += j;
-                    break;
+        void init(int n, int k, int[][] map) {
+            this.n = n;
+            this.k = k;
+            this.map = map;
+            this.h = 0;
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    h = Math.max(h, map[r][c]);
+                    visited[r][c] = false;
                 }
             }
         }
-        visited[r][c] = false;
-        return 1 + temp;
-    }
 
-    private int solve() {
-        int answer = 0;
-        for (int r=0; r<n; r++) {
-            for (int c=0; c<n; c++) {
-                if (map[r][c] == h) {
-                    answer = Math.max(answer, getMaxPath(r, c, false));
-//                    System.out.println(String.format("%d %d: %d", r, c, getMaxPath(r, c, false)));
+        boolean valid(int r, int c) {
+            return r >= 0 && r < n && c >= 0 && c < n;
+        }
+
+        int getMaxPath(int r, int c, boolean cheat) {
+            visited[r][c] = true;
+            int temp = 0;
+            for (int i = 0; i < 4; i++) {
+                int mr = r + MR[i];
+                int mc = c + MC[i];
+                if (!valid(mr, mc)) continue;
+                if (visited[mr][mc]) continue;
+                if (map[mr][mc] < map[r][c]) {
+                    temp = Math.max(temp, getMaxPath(mr, mc, cheat));
+                    continue;
+                }
+                if (cheat) continue;
+                for (int j = 1; j <= k; j++) {
+                    if (map[mr][mc] - j >= 0 && map[mr][mc] - j < map[r][c]) {
+                        map[mr][mc] -= j;
+                        temp = Math.max(temp, getMaxPath(mr, mc, true));
+                        map[mr][mc] += j;
+                        break;
+                    }
                 }
             }
+            visited[r][c] = false;
+            return 1 + temp;
         }
-        return answer;
+
+        int resolve() {
+            int answer = 0;
+            for (int r = 0; r < n; r++) {
+                for (int c = 0; c < n; c++) {
+                    if (map[r][c] == h) {
+                        answer = Math.max(answer, getMaxPath(r, c, false));
+                    }
+                }
+            }
+            return answer;
+        }
     }
-
-    private int n, k, h;
-    private int[][] map = new int[8][8];
-    private boolean[][] visited = new boolean[8][8];
-
-    private static final int[] MR = {0, 0, -1, 1};
-    private static final int[] MC = {-1, 1, 0, 0};
 
 }
--- week01/dhparkhub/problem1952/Solution.java
+++ week01/dhparkhub/problem1952/Solution.java
@@ -5,35 +5,49 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Resolver resolver = new Resolver();
+        int t;
+        int[] cost = new int[4];
+        int[] plan = new int[12];
+
         Scanner scanner = new Scanner(System.in);
-        int tc = scanner.nextInt();
-        for (int i = 1; i <= tc; i++) {
+        t = scanner.nextInt();
+        for (int i = 1; i <= t; i++) {
             for (int j = 0; j < 4; j++) {
-                solution.cost[j] = scanner.nextInt();
+                cost[j] = scanner.nextInt();
             }
             for (int j = 0; j < 12; j++) {
-                solution.plan[j] = scanner.nextInt();
+                plan[j] = scanner.nextInt();
             }
-            int answer = solution.solve();
+            resolver.init(cost, plan);
+            int answer = resolver.resolve();
             System.out.println(String.format("#%d %d", i, answer));
         }
     }
 
-    private int getMinCost(int month, int total) {
-        if (month >= 12) return total;
-        if (plan[month] == 0) return getMinCost(month + 1, total);
-        int case1 = getMinCost(month + 1, total + plan[month] * cost[0]);
-        int case2 = getMinCost(month + 1, total + cost[1]);
-        int case3 = getMinCost(month + 3, total + cost[2]);
-        return Math.min(case1, Math.min(case2, case3));
-    }
+    static class Resolver {
 
-    private int solve() {
-        return Math.min(cost[3], getMinCost(0, 0));
-    }
+        int[] cost = new int[4];
+        int[] plan = new int[12];
 
-    private int[] cost = new int[4];
-    private int[] plan = new int[12];
+        void init(int[] cost, int[] plan) {
+            this.cost = cost;
+            this.plan = plan;
+        }
+
+        int getMinCost(int month, int total) {
+            if (month >= 12) return total;
+            if (plan[month] == 0) return getMinCost(month + 1, total);
+            int case1 = getMinCost(month + 1, total + plan[month] * cost[0]);
+            int case2 = getMinCost(month + 1, total + cost[1]);
+            int case3 = getMinCost(month + 3, total + cost[2]);
+            return Math.min(case1, Math.min(case2, case3));
+        }
+
+        int resolve() {
+            return Math.min(cost[3], getMinCost(0, 0));
+        }
+
+    }
 
 }
