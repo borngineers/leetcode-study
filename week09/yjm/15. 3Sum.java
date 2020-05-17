0b3d18fa955975622fc0066695cf9d0941f794d3
YJM <jmyoon4488@gmail.com>
2020-05-18 00:36:04 +0900

    Update 15. 3Sum.java

--- week09/yjm/15. 3Sum.java
+++ week09/yjm/15. 3Sum.java
@@ -73,5 +73,30 @@ public class LeetCode15 {
 
             return retList;
         }
+        
+        public List<List<Integer>> threeSum3(int[] nums) {
+            List<List<Integer>> retList = new ArrayList<>();
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 3; i++) {
+                if (i > 0 && nums[i] == nums[i-1]) { continue; }
+                int j = i + 1;
+                int k = nums.length - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        retList.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        j++;
+                        k--;
+                        while (j < k && nums[j] == nums[j-1]) { j++; }
+                        while (j < k && nums[k] == nums[k+1]) { k--; }
+                    } else if (sum < 0) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+            return retList;
+        }
     }
 }
