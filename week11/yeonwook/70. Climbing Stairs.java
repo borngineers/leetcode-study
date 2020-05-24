97874f6a45593b252d7bd2af3631d7b2d8f11325
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-05-24 18:08:09 +0900

    Update 70. Climbing Stairs.java

--- week11/yeonwook/70. Climbing Stairs.java
+++ week11/yeonwook/70. Climbing Stairs.java
@@ -25,3 +25,27 @@ class Solution {
         return result;
     }
 }
+
+
+---
+    
+class Solution {
+    
+    Map<Integer, Integer> map = new HashMap<>();
+    
+    public int climbStairs(int n) {
+        
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        
+    	if(n <= 0) return -1;
+        
+        if(n == 1 || n==2) return n;
+        
+        int result = climbStairs(n-1) + climbStairs(n-2);
+        map.put(n, result);
+        
+        return result;
+    }
+}
