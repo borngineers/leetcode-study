4bc6d8dfa2b950c26835b9c570315e25a801bdd1
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-05-24 18:07:05 +0900

    Create 70. Climbing Stairs.java

---
+++ week11/yeonwook/70. Climbing Stairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    Map<Integer, Integer> map = new HashMap<>();
+    
+    public int climbStairs(int n) {
+        
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        
+        if(n < 0) return -1;
+        if(n==1 || n==2) return n;
+        
+    	  int result = 0;
+        int climb1 = 1;
+        int climb2 = 2;
+        
+        for(int i=3; i<=n; i++) {
+            result = climb1 + climb2;
+            climb1 = climb2;
+            climb2 = result;
+        }
+        
+        map.put(n, result);
+        return result;
+    }
+}
