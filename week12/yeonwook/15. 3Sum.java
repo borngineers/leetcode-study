0f637d1612f7af01f6cd465aef4ad1352614f1d0
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-06-15 21:43:31 +0900

    Create 15. 3Sum.java

---
+++ week12/yeonwook/15. 3Sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> list = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-1; i++) {
+            
+            if(i>0 && nums[i-1] == nums[i]) continue;
+            
+            int left = i+1;
+            int right = nums.length - 1;
+            
+            while(left < right) {
+                
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum == 0) {
+                    
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    
+                    while(nums[right] == nums[right+1] && left < right) right--;
+                    while(nums[left] == nums[left-1] && left < right) left++;
+                    
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return list;
+    }
+}
