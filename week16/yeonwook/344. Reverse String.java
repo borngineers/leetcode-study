85ae93f2a76e5999f9b6a097d43b181ff0f8c4e1
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-06-30 22:20:46 +0900

    Create 344. Reverse String.java

---
+++ week16/yeonwook/344. Reverse String.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int len = s.length;
+        if(len == 0 || len == 1) {
+            return ;
+        }
+        
+        int left = 0;
+        int right = len - 1;
+        
+        while(left < right) {
+            
+            char temp = s[left];
+            
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+}
