1936b42d5d8730e978b41e0dcd30c6da0d747d93
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-07-04 15:53:29 +0900

    Update 198. House Robber.java

--- week17/yeonwook/198. House Robber.java
+++ week17/yeonwook/198. House Robber.java
@@ -2,6 +2,28 @@ class Solution {
     public int rob(int[] nums) {
         
         if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        
+        int a = nums[0];
+        int b = nums[1];
+        int answer = 0;
+        
+        for(int i=2; i<nums.length; i++) {
+            
+            answer = Math.max(a+nums[i], b);
+            a = b;
+            b  = answer;
+        }
+        
+        return answer;
+    }
+}
+
+class Solution {
+    public int rob(int[] nums) {
+        
+        if(nums.length == 0) return 0;
         
         int odd = 0;
         int even = 0;
