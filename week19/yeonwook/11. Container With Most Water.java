e9483735f19f0eba5f4c72cb3e790c1b395b7e60
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-08-02 16:07:47 +0900

    Update 11. Container With Most Water.java

--- week19/yeonwook/11. Container With Most Water.java
+++ week19/yeonwook/11. Container With Most Water.java
@@ -4,6 +4,26 @@ class Solution {
         if(height.length <= 1) return  0;
         
         int answer = 0;
+        int left = 0;
+        int right = height.length - 1;
+        
+        while(left<right) {
+            answer = Math.max(answer, (right-left)*Math.min(height[left], height[right]));
+            
+            if(height[left] < height[right]) left++;
+            else right--;
+        }
+        
+        return answer;
+    }
+}
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        if(height.length <= 1) return  0;
+        
+        int answer = 0;
         for(int i=0; i<height.length; i++) {
             for(int j=0; j<height.length; j++) {
                 if(i==j) continue;
