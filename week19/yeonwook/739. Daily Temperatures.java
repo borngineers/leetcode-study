abde601aca3ab67992dd56090ff5647beb0a756d
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-08-04 23:03:06 +0900

    Update 739. Daily Temperatures.java

--- week19/yeonwook/739. Daily Temperatures.java
+++ week19/yeonwook/739. Daily Temperatures.java
@@ -16,3 +16,21 @@ class Solution {
         return answer;
     }
 }
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        
+        int[] answer = new int[T.length];
+        
+        for(int i=0; i<T.length; i++) {
+            for(int j=i; j<T.length; j++) {
+                if(T[i]<T[j]) {
+                    answer[i] = j-i;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
