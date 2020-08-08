0a89695fefb788d177ac37e5d6b0e598b1b60125
yeonwook2 <52685504+yeonwook2@users.noreply.github.com>
2020-08-08 16:25:32 +0900

    Update 215. Kth Largest Element in an Array.java

--- week20/yeonwook/215. Kth Largest Element in an Array.java
+++ week20/yeonwook/215. Kth Largest Element in an Array.java
@@ -1,1 +1,11 @@
-test
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        Arrays.sort(nums);
+        
+        int target = nums.length-k;
+        
+        return nums[target];
+        
+    }
+}
